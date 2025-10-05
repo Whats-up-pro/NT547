3e3d2fd20bd4eb46fe0da32903cb25af53d4eb51
614_2U0C <147789234+Whats-up-pro@users.noreply.github.com>
2025-10-05 23:29:05 +0700

    improve display in editor panel

--- frontend/src/components/CodeEditor.js
+++ frontend/src/components/CodeEditor.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CodeEditor.css';
 
 function CodeEditor({ value, onChange, selectedNode }) {
+  const [cursorPos, setCursorPos] = useState({ line: 1, col: 1 });
+
+  const updateCursorFromEvent = (e) => {
+    const pos = e.target.selectionStart || 0;
+    const textUpToPos = e.target.value.slice(0, pos);
+    const line = textUpToPos.split('\n').length; // 1-based
+    const lastNewlineIndex = textUpToPos.lastIndexOf('\n');
+    const col = pos - (lastNewlineIndex + 1) + 1; // 1-based
+    setCursorPos({ line, col });
+  };
+
   const handleChange = (e) => {
     onChange(e.target.value);
+    updateCursorFromEvent(e);
   };
 
   return (
@@ -11,14 +23,18 @@ function CodeEditor({ value, onChange, selectedNode }) {
       <textarea
         value={value}
         onChange={handleChange}
+        onSelect={updateCursorFromEvent}
+        onKeyUp={updateCursorFromEvent}
+        onClick={updateCursorFromEvent}
         className="code-textarea"
         spellCheck="false"
       />
-      {selectedNode && (
-        <div className="selected-info">
-          Selected: Lines {selectedNode.startLine} - {selectedNode.endLine}
-        </div>
-      )}
+      <div className="selected-info">
+        <div>Current line:  {cursorPos.line}</div>
+        {selectedNode && (
+          <div>Selected: Lines {selectedNode.startLine} - {selectedNode.endLine}</div>
+        )}
+      </div>
     </div>
   );
 }
